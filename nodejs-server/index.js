1bb46b557c7bc56c6bf70267a6b68cae1116d97c
Nitiraj Rathore <nrathore@cloudera.com>
2019-06-21 21:53:32 +0530

    some examples for simple kubernetes and a multi node cluster of kubernetes using dind

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var app = express();
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+var id = getRandomInt(10000);
 var count = 0;
 var host;
 var port;
 app.get("/", function(req, res){
     count++;
-    console.log("request number : " + count);
-    res.send(count + "." + 'Hello World! from ' + host + ":" + port);
+    console.log(id + ". request number : " + count);
+    res.send(count + ". " + 'Hello World! from server-id: ' + id);
 });
 
 var server = app.listen(3000, function(){

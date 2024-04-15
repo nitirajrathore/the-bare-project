925d4c9bc33e3637baf3b78fc252e00c6fa90ba2
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2024-04-15 15:18:48 +0530

    go-entc: adding the entity files auto generated by `ent` when I ran `go generate ./ent`

--- go/go-entc/ent/ent.go
+++ go/go-entc/ent/ent.go
@@ -6,6 +6,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"go-entc/ent/car"
+	"go-entc/ent/group"
 	"go-entc/ent/user"
 	"reflect"
 	"sync"
@@ -73,7 +75,9 @@ var (
 func checkColumn(table, column string) error {
 	initCheck.Do(func() {
 		columnCheck = sql.NewColumnCheck(map[string]func(string) bool{
-			user.Table: user.ValidColumn,
+			car.Table:   car.ValidColumn,
+			group.Table: group.ValidColumn,
+			user.Table:  user.ValidColumn,
 		})
 	})
 	return columnCheck(table, column)

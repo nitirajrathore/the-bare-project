70d456646b0eb7758c3aa540b88016988dfc2d6a
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2024-08-30 23:07:29 +0530

    go-rod-test: adding some tests for downloading pdfs using go-rod which works fine.

--- go/go-rod-test/download_zip.go
+++ go/go-rod-test/download_zip.go
@@ -1,1 +1,98 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/proto"
+)
+
+// WORKS FINE
+func main_download_zip_works() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url := "https://nsearchives.nseindia.com/corporate/d_Matrimony.zip"
+	browser := rod.New().MustConnect()
+	browserPage := browser.MustPage()
+
+	wait := browserPage.Browser().Context(context.Background()).WaitDownload(wd)
+
+	go browser.EachEvent(func(e *proto.PageDownloadProgress) bool {
+		completed := "(unknown)"
+		if e.TotalBytes != 0 {
+			completed = fmt.Sprintf("%0.2f%%", e.ReceivedBytes/e.TotalBytes*100.0)
+		}
+		log.Printf("state: %s, completed: %s\n", e.State, completed)
+		return e.State == proto.PageDownloadProgressStateCompleted
+	})()
+	// Navigate to the page with the ZIP file link
+
+	err = browserPage.Navigate(url)
+	if err != nil && !strings.Contains(err.Error(), "net::ERR_ABORTED") {
+		// Note: Ignoring the net::ERR_ABORTED page error is essential here
+		// since downloads will cause this error to be emitted, although the
+		// download will still succeed.
+		log.Printf("Error occurred while creating page for url : %s using rod: %v", url, err)
+		return
+	}
+	defer browserPage.Close()
+
+	res := wait()
+	log.Printf("wrote %s", filepath.Join(wd, res.GUID))
+}
+
+func main_not_working() {
+	// get working directory
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url := "https://nsearchives.nseindia.com/corporate/d_Matrimony.zip"
+	browser := rod.New().MustConnect()
+
+	browserPage, err := browser.Page(proto.TargetCreateTarget{
+		URL: url,
+	})
+
+	if err != nil && !strings.Contains(err.Error(), "net::ERR_ABORTED") {
+		// Note: Ignoring the net::ERR_ABORTED page error is essential here
+		// since downloads will cause this error to be emitted, although the
+		// download will still succeed.
+		log.Printf("Error occurred while creating page for url : %s using rod: %v", url, err)
+		return
+	}
+	defer browserPage.Close()
+
+	//page.MustElementR("summary", "Code").MustClick()
+
+	wait := browserPage.Browser().Context(context.Background()).WaitDownload(wd)
+
+	//browserPage.Browser().Context(context.Background()).wa
+	go browser.EachEvent(func(e *proto.PageDownloadProgress) bool {
+		completed := "(unknown)"
+		if e.TotalBytes != 0 {
+			completed = fmt.Sprintf("%0.2f%%", e.ReceivedBytes/e.TotalBytes*100.0)
+		}
+		log.Printf("state: %s, completed: %s\n", e.State, completed)
+		return e.State == proto.PageDownloadProgressStateCompleted
+	})()
+
+	//page.MustElementR("a", "Download ZIP").MustClick()
+	//err = browserPage.WaitLoad()
+	//if err != nil {
+	//	log.Printf("error occurred while loading page %s: %v", url, err)
+	//	return
+	//}
+	res := wait()
+
+	log.Printf("wrote %s", filepath.Join(wd, res.GUID))
+}
--- go/go-rod-test/test_download_file.go
+++ go/go-rod-test/test_download_file.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/go-rod/rod/lib/proto"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-rod/rod"
 )
@@ -64,8 +69,8 @@ func main3() {
 	log.Print("wrote ndownload.pdf")
 }
 
-// THis too works. Awesome.
-func main() {
+// NOT WORKING.
+func main_bseindia_not_working() {
 	u := "https://www.bseindia.com/bseplus/AnnualReport/543258/74183543258.pdf"
 
 	browser := rod.New().MustConnect()
@@ -80,5 +85,50 @@ func main() {
 	if err := os.WriteFile("download.pdf", b, 0o644); err != nil {
 		log.Fatal(err)
 	}
-	log.Print("wrote ndownload.pdf")
+	log.Print("wrote download.pdf")
+}
+
+func main_download_pdf_with_progress() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url := "https://www.bseindia.com/bseplus/AnnualReport/543258/74183543258.pdf"
+	browser := rod.New().MustConnect()
+	browserPage := browser.MustPage()
+
+	wait := browserPage.Browser().Context(context.Background()).WaitDownload(wd)
+
+	go browser.EachEvent(func(e *proto.PageDownloadProgress) bool {
+		completed := "(unknown)"
+		if e.TotalBytes != 0 {
+			completed = fmt.Sprintf("%0.2f%%", e.ReceivedBytes/e.TotalBytes*100.0)
+		}
+		log.Printf("state: %s, completed: %s\n", e.State, completed)
+		return e.State == proto.PageDownloadProgressStateCompleted
+	})()
+	// Navigate to the page with the ZIP file link
+
+	err = browserPage.Navigate(url)
+	if err != nil && !strings.Contains(err.Error(), "net::ERR_ABORTED") {
+		// Note: Ignoring the net::ERR_ABORTED page error is essential here
+		// since downloads will cause this error to be emitted, although the
+		// download will still succeed.
+		log.Printf("Error occurred while creating page for url : %s using rod: %v", url, err)
+		return
+	}
+	defer browserPage.Close()
+
+	err = browserPage.WaitLoad()
+	if err != nil {
+		log.Printf("Error occurred while waiting for page to load : %s using rod: %v", url, err)
+		return
+	}
+	res := wait()
+	log.Printf("wrote %s", filepath.Join(wd, res.GUID))
+}
+
+func main() {
+	main_download_pdf_with_progress()
 }

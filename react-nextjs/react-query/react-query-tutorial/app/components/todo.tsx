a6d50300b2ffcc007ecb5b137a3563aa7c4853fb
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2025-01-08 21:08:41 +0530

    react-query:fixed the rendering undefined bug which caused key error in <ul> <li>

--- react-nextjs/react-query/react-query-tutorial/app/components/todo.tsx
+++ react-nextjs/react-query/react-query-tutorial/app/components/todo.tsx
@@ -4,14 +4,28 @@ export default function Todo() {
     const todosIdsQuery = useTodosIds();
     const todosQueries = useTodos(todosIdsQuery.data);
 
+    // Early return if the todosIdsQuery is still loading or has an error
     if (todosIdsQuery.isPending) {
-        return <span> loading. ...</span>;
+        return <span> loading... </span>;
     }
 
     if (todosIdsQuery.isError) {
-        return <span> there is an error !</span>;
+        return <span> there is an error! </span>;
     }
 
+    // Ensure todosIdsQuery.data is defined before using it
+    if (!todosIdsQuery.data) {
+        return <span> No data available. </span>;
+    }
+
+    // Ensure todosQueries is fully loaded and contains data
+    if (!todosQueries || todosQueries.some(query => query.isPending)) {
+        return <span> loading... </span>;
+    }
+
+    if (todosQueries.some(query => query.isError)) {
+        return <span> there is an error with one or more todos! </span>;
+    }
 
     return (
         <>
@@ -20,8 +34,8 @@ export default function Todo() {
                     <li key={data?.id}>
                         <div>Id: {data?.id}</div>
                         <span>
-                            <strong> Title: </strong> {data?.title}, {" "}
-                            <strong>Description: </strong> {data?.description}
+                            <strong>Title:</strong> {data?.title},{" "}
+                            <strong>Description:</strong> {data?.description}
                         </span>
                     </li>
                 ))}

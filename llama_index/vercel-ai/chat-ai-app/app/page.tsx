024f2d25563c8df37ca8f3f37087c55593f16e23
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2025-03-01 11:11:55 +0530

    chat-ai-app: adding events in useChat()

--- llama_index/vercel-ai/chat-ai-app/app/page.tsx
+++ llama_index/vercel-ai/chat-ai-app/app/page.tsx
@@ -4,10 +4,25 @@ import { useChat } from '@ai-sdk/react';
 import Spinner from '@/components/spinner';
 import { useState, useEffect } from 'react';
 import { Trash2 } from 'lucide-react';
+import { Input } from '@/components/shadcn/input';
+import { Button } from '@/components/shadcn/button';
+
 
 export default function Page() {
   const { messages, setMessages, input, handleInputChange, handleSubmit, status, stop, error, reload } =
-    useChat({});
+    useChat({
+      onFinish: (message, { usage, finishReason }) => {
+        console.log('Finished streaming message:', message);
+        console.log('Token usage:', usage);
+        console.log('Finish reason:', finishReason);
+      },
+      onError: error => {
+        console.error('An error occurred:', error);
+      },
+      onResponse: response => {
+        console.log('Received HTTP response from server:', response);
+      },
+    });
 
   const [localError, setLocalError] = useState(error);
 
@@ -71,18 +86,18 @@ export default function Page() {
         )}
 
         <form onSubmit={handleSubmit} className="mt-4 flex flex-col items-center">
-          <input
+          <Input
             name="prompt"
             value={input}
             onChange={handleInputChange}
             className="w-full p-2 mb-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white"
           />
-          <button
+          <Button
             type="submit"
             className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 dark:bg-purple-500 dark:hover:bg-purple-600"
           >
             Submit
-          </button>
+          </Button>
         </form>
       </div>
     </div>

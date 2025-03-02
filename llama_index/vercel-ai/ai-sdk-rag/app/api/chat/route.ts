c45fb50a63b70d090ce967d3061a4e9dcd79331a
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2025-03-02 18:46:43 +0530

    ai-sdk-rag: adding code that will add my personal information in db automatically during conversation. I think gpt-4o works better than gpt-4o-mini at this task.

---
+++ llama_index/vercel-ai/ai-sdk-rag/app/api/chat/route.ts
@@ -0,0 +1,30 @@
+import { createResource } from '@/lib/actions/resources';
+import { openai } from '@ai-sdk/openai';
+import { streamText, tool } from 'ai';
+import { z } from 'zod';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: openai('gpt-4o'),
+    messages,
+    tools: {
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        parameters: z.object({
+          content: z
+            .string()
+            .describe('the content or resource to add to the knowledge base'),
+        }),
+        execute: async ({ content }) => createResource({ content }),
+      }),
+    },
+  });
+
+  return result.toDataStreamResponse();
+}

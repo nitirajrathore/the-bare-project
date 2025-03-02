de02b8c59823d9480c1feeb69a0d0c0227cc65d5
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2025-03-02 12:58:08 +0530

    ai-tool-calls: adding streaming of tool calls.

--- llama_index/vercel-ai/ai-tool-calls/app/api/custom-chat/route.ts
+++ llama_index/vercel-ai/ai-tool-calls/app/api/custom-chat/route.ts
@@ -18,6 +18,7 @@ export async function POST(req: Request) {
       model: openai('gpt-4o-mini'),
       system: 'You are a helpful assistant.',
       messages,
+      toolCallStreaming: true,
       tools: {
         // server-side tool with execute function:
         getWeatherInformation: {

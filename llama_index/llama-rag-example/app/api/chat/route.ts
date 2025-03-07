377b92d697524c020bcf47c5c86410c6197bcc2c
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2025-03-07 11:07:42 +0530

    llama-rag-example/llama-rag-example: working events. And example of token couting, (may be incorrect). I had to remove the vercel stream to allow eventing to work on default stream. Need to fix this somehow, because it removes the feature of suggesting questions.

--- llama_index/llama-rag-example/app/api/chat/route.ts
+++ llama_index/llama-rag-example/app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import { initObservability } from "@/app/observability";
 import { LlamaIndexAdapter, Message, StreamData } from "ai";
-import { ChatMessage, Settings } from "llamaindex";
+import { ChatMessage, Settings, extractText } from "llamaindex";
 import { NextRequest, NextResponse } from "next/server";
 import { createChatEngine } from "./engine/chat";
 import { initSettings } from "./engine/settings";
@@ -11,24 +11,55 @@ import {
 } from "./llamaindex/streaming/annotations";
 import { createCallbackManager } from "./llamaindex/streaming/events";
 import { generateNextQuestions } from "./llamaindex/streaming/suggestion";
+import { encodingForModel } from "js-tiktoken";
 
 initObservability();
 initSettings();
+const encoding = encodingForModel("gpt-4o-mini");
+let tokenCount = 0;
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
 Settings.callbackManager.on("llm-start", (event) => {
-  console.log("llm-start:", event.detail);
+  console.log("llm-start:", event.detail.messages);
 });
 
 Settings.callbackManager.on("llm-end", (event) => {
-  console.log("llm-end:", event.detail);
+  console.log("llm-end:", event.detail.response.message);
+});
+
+Settings.callbackManager.on("llm-start", (event) => {
+  const { messages } = event.detail;
+  messages.reduce((count: number, message: ChatMessage) => {
+    return count + encoding.encode(extractText(message.content)).length;
+  }, 0);
+  console.log("llm-start: Token count:", tokenCount);
+  // https://openai.com/pricing
+  // $10.00 / 1M tokens
+  // console.log(`Total Price: $${(tokenCount / 1_000_000) * 10}`);
 });
 
 Settings.callbackManager.on("llm-stream", (event) => {
-  console.log("llm-stream:", event.detail);
+  const { chunk } = event.detail;
+  const { delta } = chunk;
+  tokenCount += encoding.encode(extractText(delta)).length;
+  console.log("llm-stream : Token count:", tokenCount);
+  // if (tokenCount > 20) {
+  //   // This is just an example, you can set your own limit or handle it differently
+  //   throw new Error("Token limit exceeded!");
+  // }
 });
+Settings.callbackManager.on("llm-end", () => {
+  // https://openai.com/pricing
+  // $30.00 / 1M tokens
+  console.log("Token count:", tokenCount);
+  // console.log(`Total Price: $${(tokenCount / 1_000_000) * 30}`);
+});
+
+// Settings.callbackManager.on("llm-stream", (event) => {
+//   console.log("llm-stream:", event.detail);
+// });
 
 Settings.callbackManager.on("query-start", (event) => {
   console.log("query-start:", event.detail);
@@ -56,7 +87,7 @@ Settings.callbackManager.on("retrieve-end", (event) => {
 
 export async function POST(request: NextRequest) {
   // Init Vercel AI StreamData and timeout
-  const vercelStreamData = new StreamData();
+  // const vercelStreamData = new StreamData();
 
   try {
     const body = await request.json();
@@ -80,38 +111,46 @@ export async function POST(request: NextRequest) {
     const userMessageContent = retrieveMessageContent(messages);
 
     // Setup callbacks
-    const callbackManager = createCallbackManager(vercelStreamData);
+    // const callbackManager = createCallbackManager(vercelStreamData);
     const chatHistory: ChatMessage[] = messages.slice(0, -1) as ChatMessage[];
 
     // Calling LlamaIndex's ChatEngine to get a streamed response
-    const response = await Settings.withCallbackManager(callbackManager, () => {
-      return chatEngine.chat({
-        message: userMessageContent,
-        chatHistory,
-        stream: true,
-      });
+    // const response = await Settings.withCallbackManager(callbackManager, () => {
+    // return chatEngine.chat({
+    //   message: userMessageContent,
+    //   chatHistory,
+    //   stream: true,
+    // });
+    // });
+
+    const response = await chatEngine.chat({
+      message: userMessageContent,
+      chatHistory,
+      stream: true,
     });
 
-    const onCompletion = (content: string) => {
-      chatHistory.push({ role: "assistant", content: content });
-      generateNextQuestions(chatHistory)
-        .then((questions: string[]) => {
-          if (questions.length > 0) {
-            vercelStreamData.appendMessageAnnotation({
-              type: "suggested_questions",
-              data: questions,
-            });
-          }
-        })
-        .finally(() => {
-          vercelStreamData.close();
-        });
-    };
+    // const onCompletion = (content: string) => {
+    //   chatHistory.push({ role: "assistant", content: content });
+    //   generateNextQuestions(chatHistory)
+    //     .then((questions: string[]) => {
+    //       if (questions.length > 0) {
+    //         vercelStreamData.appendMessageAnnotation({
+    //           type: "suggested_questions",
+    //           data: questions,
+    //         });
+    //       }
+    //     })
+    //     .finally(() => {
+    //       vercelStreamData.close();
+    //     });
+    // };
 
-    return LlamaIndexAdapter.toDataStreamResponse(response, {
-      data: vercelStreamData,
-      callbacks: { onCompletion },
-    });
+    return LlamaIndexAdapter.toDataStreamResponse(response
+      //   , {
+      //   data: vercelStreamData,
+      //   callbacks: { onCompletion },
+      // }
+    );
   } catch (error) {
     console.error("[LlamaIndex]", error);
     return NextResponse.json(

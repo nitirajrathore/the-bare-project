697251bb18612b65662640bb6849713d3d78834a
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2025-04-12 10:03:25 +0530

    the-bare-project/crawl4ai: adding a simple quickstart examples of crawl4ai

---
+++ crawl4ai/webscraping_example/README.md
@@ -0,0 +1,16 @@
+
+## Install crawl4ai
+Source : https://docs.crawl4ai.com/core/installation/
+Install
+```
+pip install crawl4ai
+```
+Setup
+```
+crawl4ai-setup
+```
+Test
+```
+crawl4ai-doctor
+```
+
---
+++ crawl4ai/webscraping_example/crawl4ai/quickstart.py
@@ -0,0 +1,39 @@
+#  source: [Quick Start - Crawl4AI Documentation (v0.5.x)](https://docs.crawl4ai.com/core/quickstart/)
+
+import asyncio
+from crawl4ai import AsyncWebCrawler
+
+async def main():
+  async with AsyncWebCrawler() as crawler: 
+    result = await crawler.arun("https://example.com")
+    print("----------------- MARKDOWN -----------------")
+    print(result.markdown[:300])
+
+    # print("----------------- FIT MARKDOWN -----------------")
+    # print(result.fit_markdown[:300])
+
+    # print("----------------- FIT HTML -----------------")
+    # print(result.fit_html[:300])
+
+    print("----------------- MARKDOWN V2 -----------------")
+    print(result.markdown_v2.fit_markdown[:300])
+
+    print("----------------- MARKDOWN V2 HTML -----------------")
+    print(result.markdown_v2.fit_html[:300])
+
+    print("----------------- MARKDOWN V2 RAW -----------------")
+    print(result.markdown_v2.raw_markdown[:300])
+
+    print("----------------- MARKDOWN V2 REFERENCES -----------------")
+    print(result.markdown_v2.references_markdown[:300])
+
+    print("----------------- HTML -----------------")
+    print(result.html[:300])
+
+    print("----------------- CLEANED HTML -----------------")
+    print(result.cleaned_html[:300])
+
+
+
+if __name__ == "__main__":
+  asyncio.run(main())
